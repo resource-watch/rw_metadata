a8c9465e3a7834627e740aff3622d56df235f999
rrequero <rareq1987@gmail.com>
2016-08-18 11:14:03 +0200

    add correct name

--- app/src/models/metadata.js
+++ app/src/models/metadata.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 const APPLICATIONS = require('appConstants').APPLICATIONS;
 
-var Dataset = new Schema({
+var Metadata = new Schema({
     dataset: {type: String, required: true, trim: true},
     application: {type: String, required: true, trim: true, enum: APPLICATIONS },
     info: {type: Schema.Types.Mixed, default: {}}
 });
 
 
-module.exports = mongoose.model('Dataset', Dataset);
+module.exports = mongoose.model('Metadata', Metadata);
